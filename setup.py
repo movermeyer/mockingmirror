c5794215ec644e9db28c640459b8dcfdfa59b265
Mark Haines <mark.haines@openmarket.com>
2015-01-02 18:01:22 +0000

    Add license

--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 def read_file(*names):
     file_path = os.path.join(here, *names)
     with open(file_path, encoding="utf-8") as f:
         return f.read()
+
 
 def exec_file(*names):
     code = read_file(*names)
     result = {}
     exec(code, result)
     return result
+
 
 setup(
     name="mockingmirror",
@@ -23,11 +26,11 @@ setup(
     url="https://github.com/NegativeMjark/mockingmirror",
     author="Mark Haines",
     author_email="mjark@negativecurvature.net",
-    license="MIT",
+    license="Apache License, Version 2.0",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 2",
     ],
     keywords="mock",
